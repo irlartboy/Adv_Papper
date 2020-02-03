b6ac79cc7dde6689ad937467d1fb18cacc3e92c2
irlartboy <ollie_king99@outlook.com>
2020-02-03 20:32:14 +1100

    first

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D cont;
+ 
+    private float horiMove = 0f;
+    public float walkSpeed = 150f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    void Update()
+    {
+        horiMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        cont.Move(horiMove * Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+}
