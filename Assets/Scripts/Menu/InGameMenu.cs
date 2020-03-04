ea6263fbd4daff5611f6a189d4600b25fefd4087
irlartboy <ollie_king99@outlook.com>
2020-03-04 12:25:56 +1100

    menus updated

--- Assets/Scripts/Menu/InGameMenu.cs
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -1,17 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
+    public GameObject pausePanel;
+    public GameObject optionsPanel;
+    public GameObject gameoverPanel;
 
-    void Start()
-    {
-        
-    }
+ 
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            if (Time.timeScale == 1)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+    public void OptionsButton()
+    {
+        pausePanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 }
